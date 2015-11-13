f8757c55872f1263a4329462d72fa8ce5f3d9363
Dan Halloran <dhalloran@classy.org>
2015-11-13 14:16:28 -0800

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113195840) do
+ActiveRecord::Schema.define(version: 20151113221058) do
 
   create_table "campaign_users", force: :cascade do |t|
     t.integer  "campaign_id", limit: 4
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20151113195840) do
 
   create_table "campaigns", force: :cascade do |t|
     t.integer  "organization_id", limit: 4
+    t.integer  "external_id",     limit: 4
     t.string   "name",            limit: 255
+    t.string   "status",          limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -43,7 +45,8 @@ ActiveRecord::Schema.define(version: 20151113195840) do
   end
 
   create_table "organizations", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.integer  "external_id", limit: 4
+    t.string   "name",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
